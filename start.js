15745e9ee269cd674a4281220bc182fd71eb6f51
EllaxElla118 <onasogaemmanuel02@gmail.com>
2024-10-02 13:09:13 +0100

    Change Remote Web Cache

--- start.js
+++ start.js
@@ -15,7 +15,7 @@ wss.on('connection', (ws) => {
             // Initialize the client with headless option
             const client = new Client({
                 webVersionCache: {
-                    remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2402.5-beta.html',
+                    remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
                     type: 'remote'
                 },
                 puppeteer: {
